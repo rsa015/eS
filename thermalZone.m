195bd40db4406e8c6114b7ab4cccf66a02294dd3
rsa <s.adjignon015@hotmail.com>
2022-04-27 17:54:07 +0000

    changed itemList to zoneItemList

--- thermalZone.m
+++ thermalZone.m
@@ -4,7 +4,7 @@ classdef thermalZone
     
     properties
         totalPower
-        itemList = containers.Map()
+        zoneItemList = containers.Map()
         zoneName
     end
     
@@ -12,12 +12,20 @@ classdef thermalZone
         function obj = thermalZone(zoneName, itemList, totalPower)
             if nargin == 3
                 obj.zoneName = zoneName;
-                obj.itemList = itemList;
+                obj.zoneItemList = zoneItemList;
                 obj.totalPower = totalPower;
             end
         end
         
         function addmultiItem(number, varargin)
+            p = inputParser;
+            validpars = @(x) isnumeric(x) && isscalar(x) && (x > 0);
+            p.addParameter('power', validpars)
+            p.addRequired('name', @isstring);
+            p.addParameter('category', @isstring);
+            p.addParameter('pw-range', @isstring);
+            p.addParameter('schedule', @isstring);
+            parse(p, varargin{:});
             if ~ number
                 
             end
