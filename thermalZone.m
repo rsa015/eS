cfb4fb217a08fe552c4d7ef8953a5cdf04bb1105
rsa <s.adjignon015@hotmail.com>
2022-04-27 17:36:52 +0000

    added class properties, obj function

--- thermalZone.m
+++ thermalZone.m
@@ -3,10 +3,25 @@ classdef thermalZone
     %
     
     properties
-        Property1
+        totalPower
+        itemList = containers.Map()
+        zoneName
     end
     
     methods
+        function obj = thermalZone(zoneName, itemList, totalPower)
+            if nargin == 3
+                obj.zoneName = zoneName;
+                obj.itemList = itemList;
+                obj.totalPower = totalPower;
+            end
+        end
+        
+        function addmultiItem(number, varargin)
+            if ~ number
+                
+            end
+        end
         
     end
 end
