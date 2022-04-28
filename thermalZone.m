3e030b78fefa604e697b5c94205b8d68e1a3a519
rsa <s.adjignon015@hotmail.com>
2022-04-28 07:10:21 +0000

    Added the addmultiItem function

--- thermalZone.m
+++ thermalZone.m
@@ -9,7 +9,7 @@ classdef thermalZone
     end
     
     methods
-        function obj = thermalZone(zoneName, itemList, totalPower)
+        function obj = thermalZone(zoneName, zoneItemList, totalPower)
             if nargin == 3
                 obj.zoneName = zoneName;
                 obj.zoneItemList = zoneItemList;
@@ -17,7 +17,8 @@ classdef thermalZone
             end
         end
         
-        function addmultiItem(number, varargin)
+        function addmultiItem(obj, number, varargin)
+            
             p = inputParser;
             validpars = @(x) isnumeric(x) && isscalar(x) && (x > 0);
             p.addParameter('power', validpars)
@@ -26,9 +27,19 @@ classdef thermalZone
             p.addParameter('pw-range', @isstring);
             p.addParameter('schedule', @isstring);
             parse(p, varargin{:});
+            
+            
+            itemStruct = struct('number', 1, 'category', p.Results.category, 'power range', p.Results.pw-range, 'schedule', p.Results.schedule);
+            
+            
             if ~ number
-                
+                obj.zoneItemList(p.Results.name) = itemStruct;
+            else
+                itemStruct.number = number;
+                obj.zoneItemList(p.Results.name) = itemStruct;
             end
+            
+            return
         end
         
     end
