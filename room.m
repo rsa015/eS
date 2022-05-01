6b4908f71603c0162b5433d8a84305c656e9b95e
rsa <s.adjignon015@hotmail.com>
2022-05-01 22:48:06 +0000

    initial commit:

---
+++ room.m
@@ -0,0 +1,13 @@
+classdef room
+    %UNTITLED3 Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        
+    end
+    
+    methods
+        
+    end
+end
+
