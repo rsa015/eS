b4b9581d04b3af4beaa304a17b0afcc4e79d1a35
rsa <s.adjignon015@hotmail.com>
2022-05-10 05:36:37 +0000

    Add addEquipemnts function

--- room.m
+++ room.m
@@ -1,13 +1,29 @@
-classdef room
+classdef room < containers.Map
     %UNTITLED3 Summary of this class goes here
     %   Detailed explanation goes here
     
     properties
-        
+       equipments containers.Map;
+       name string;
     end
     
     methods
+        function obj = room(name, equipments)
+            obj.name = name;
+            obj.equipments = equipments;
+            
+            obj(obj.name) = obj.equipments;
+            
+            return
+        end
         
+        function addEquipments(obj, item)
+            arguments
+                obj
+                item electricalGadget
+            end
+            obj.equipments(item.name) = item.getProps();
+        end
     end
 end
 
