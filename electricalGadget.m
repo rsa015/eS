51966fb0fb224580f18a1a2b83e13d2e320628b2
rsa <s.adjignon015@hotmail.com>
2022-05-11 20:07:57 +0000

    Deleted the getSchedule, getProps, representItem functions

--- electricalGadget.m
+++ electricalGadget.m
@@ -2,26 +2,19 @@ classdef electricalGadget
     
     properties
         name char
-        number double {mustBeNumeric}
         power double {mustBeNumeric}
         category string {mustBeMember(category, {'cooling', 'heating', 'lighting', 'equipment'})}
         prange string
-        schedule string
+        allEquips (1,:) cell
     end
     
     methods
-%         function this = electricalGadget(name, number, power, prange, category, schedule)
-%             
-%         end
-        function obj = electricalGadget(name, number, power, prange, category, schedule)
-            obj.name = name;
-            obj.number = number;
-            obj.power = power;
+        function obj = electricalGadget(name, power, prange,category)
+            obj.name = upper(name);
             obj.prange = prange;
+            obj.power = power;
             obj.category = category;
-            obj.schedule = schedule;
-%             obj = containers.Map(obj.name, struct('name', obj.name, 'number', obj.number, 'prange', obj.prange, 'caetgory', obj.category, 'schedule', obj.schedule));
-            
+            obj.allEquips{end+1} = obj.name;
             return
         end
         
@@ -39,46 +32,6 @@ classdef electricalGadget
            end
            name = obj.name;
            return
-        end
-        
-        function schedule = getSchedule(obj, schHolder)
-            arguments
-               obj electricalGadget
-               schHolder cell;
-            end
-            schedules = obj.schedule;
-            scheduleRange = schedules.split(";");
-            for i = 1:length(scheduleRange)
-                indvTime = scheduleRange(i).split("-");
-                
-                sStr = sprintf('%s', indvTime(1));
-%                 disp(sStr);
-                sTime = strcat(sStr(1:2), ":", sStr(3:4));
-                eStr = sprintf('%s', indvTime(2)); 
-                eTime = strcat(eStr(1:2), ":", eStr(3:4));
-%                 sTime = 
-                startT = duration(sTime, 'InputFormat', 'hh:mm');
-                endT = duration(eTime, 'InputFormat', 'hh:mm');
-                schHolder{i} = [startT, endT];
-            end
-            schedule = schHolder;
-            return 
-            
-        end
-        function properties = getProps(obj)
-            arguments
-               obj electricalGadget 
-            end
-            properties = struct('number', obj.number, 'power', obj.power, 'prange', obj.prange, 'category', obj.category, 'schedule', obj.schedule);
-            
-            return
-        end
-        function repr = representItem(obj)
-            arguments
-                obj electricalGadget
-            end
-            repr = struct('name', obj.name, 'power', obj.power, 'prange', obj.prange, 'category',obj.category, 'schedule', obj.schedule);
-            return
         end
     end
 end
